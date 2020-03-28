156f934cbec6a58afad0b0733d56dcb02296fdc9
jzamora5 <jzamora_5@yahoo.com>
2020-03-28 20:34:20 +0000

    [Change] Init Kwarg validation

--- models/base_model.py
+++ models/base_model.py
@@ -26,6 +26,10 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
