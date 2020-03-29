92e198629a94e62c812e71558b4b1f53e1a8de8f
jzamora5 <jzamora_5@yahoo.com>
2020-03-29 16:16:49 +0000

    [ADD] Task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -47,7 +47,7 @@ class DBStorage:
         dic = {}
         if not cls:
             # tables = [User, State, City, Amenity, Place, Review]
-            tables = [State, City]
+            tables = [User, State, City]
 
         else:
             if type(cls) == str:
