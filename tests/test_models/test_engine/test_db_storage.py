e9e6a7fcc31fbf9f0425fd0034076cc8e69a7668
mateo-a <alexander.urrego@gmail.com>
2020-03-31 13:35:55 -0500

    [FIX] Fix test_db_storage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,21 +1,5 @@
 #!/usr/bin/python3
 """test for console"""
-import unittest
-from unittest.mock import patch
-from io import StringIO
-from console import HBNBCommand
-from models.base_model import BaseModel, Base
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.engine.db_storage import DBStorage
-import MySQLdb
-from unittest.mock import patch
-import os
-from os import environ
 
 
 @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != "db", "No apply for db")
