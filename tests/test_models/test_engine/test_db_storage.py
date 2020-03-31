c3587c844d9cbb84d4718ac61512267167e9cb0b
mateo-a <alexander.urrego@gmail.com>
2020-03-31 14:02:47 -0500

    [FIX] Fix test_db_storage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -10,6 +10,8 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
+from models.review import Review
+from models.engine.db_storage import DBStorage
 
 
 @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != "db", "No apply for db")
