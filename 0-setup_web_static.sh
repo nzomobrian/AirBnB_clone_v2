904b242f88eb204f80c484a3fdfd1a88b772f51d
jzamora5 <jzamora_5@yahoo.com>
2020-04-07 15:37:47 +0000

    Fix Alias

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -16,16 +16,15 @@ CONTENT="\
   <body>
     Holberton School
   </body>
-</html>
-"
+</html>"
+
 echo "$CONTENT" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 
-
 cont="server_name _;\n\t"
 
-loc="location = \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex off;\n\t}"
+loc="location \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex off;\n\t}"
 
 sed -i "s/server_name _;/$cont$loc/" /etc/nginx/sites-enabled/default
 service nginx restart
