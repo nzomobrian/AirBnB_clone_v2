c225b8f55a2c0c4ba52cc9cc903f4d25b8d70c2d
jzamora5 <jzamora_5@yahoo.com>
2020-04-20 18:11:41 +0000

    Move teardown up

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,18 +8,18 @@ app = Flask(__name__)
 # app.jinja_env.lstrip_blocks = True
 
 
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ displays a HTML page with a list of states """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     return render_template('7-states_list.html', states=states)
-
-
-@app.teardown_appcontext
-def close_db(error):
-    """ Remove the current SQLAlchemy Session """
-    storage.close()
 
 
 if __name__ == "__main__":
